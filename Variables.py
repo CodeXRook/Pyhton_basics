234d65c53a4d81ebc5c8b4197812ee5c9a039832
CodeXRook <lacekitfly@gmail.com>
2019-04-16 02:20:02 -0400

    item1,input

--- Variables.py
+++ Variables.py
@@ -36,7 +36,5 @@ print("fri" in today)#TRUE, THERE IS A DAY IN FRIDAY // #WORKS IN INTELLIJ
 print("thur" in today)#FALSE, THERE IS NO THUR IN FRIDAY // #WORKS IN INTELLIJ
 print("parrot" in "fjord")#FALSE, THERE IS NO SUCH WORD FOUND// #WORKS IN INTELLIJ
 
-# Create five variables,
-# set them to strings that represent 5 common shopping list items 
-#I DID THIS ALREADY, SO TIME I'LL BE DOING THE INPUT SO I BETTER UNDERSTAND THE USAGE.
+
 
--- Variablespt2.py
+++ Variablespt2.py
@@ -1,2 +1,5 @@
-#Create five variables,
-#set them to strings that represent 5 common shopping list items
+# Create five variables,
+# set them to strings that represent 5 common shopping list items 
+#I DID THIS ALREADY, SO TIME I'LL BE DOING THE INPUT SO I BETTER UNDERSTAND THE USAGE.
+
+item_name = input('Add to list')
