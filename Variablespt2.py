6308c3d4f438be7b3b2ef91ed037f5d336ac097d
CodeXRook <lacekitfly@gmail.com>
2019-04-16 02:24:01 -0400

    print all

--- Variablespt2.py
+++ Variablespt2.py
@@ -4,4 +4,5 @@
 
 item_name_1 = input('Add to list') #WORKS IN INTELLIJ
 item_price_1 = input('Whats your spending budget?') #WORKS IN INTELLIJ
-item_quant_1 = input('What was the quanity amount')
+item_quant_1 = input('How much you need?') #WORKS IN INTELLIJ
+print(str(item_quant_1) + '  '+ item_name_1 +'  ' + str(item_price_1))#WORKS IN INTELLIJ
