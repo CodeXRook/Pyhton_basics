634bc727e9a9bcd3f0eca8bcc7efb5d34856af83
CodeXRook <lacekitfly@gmail.com>
2019-04-16 02:00:05 -0400

    variable Multiply strings

--- Variables.py
+++ Variables.py
@@ -1,25 +1,28 @@
 parrot = "Nowregian Blue"
-print(parrot)#WORKES IN INTELLIJ
-print(parrot[0])#WORKES IN INTELLIJ
-print(parrot[3])#WORKES IN INTELLIJ
-print(parrot[-1])#WORKES IN INTELLIJ
+print(parrot)#WORKS IN INTELLIJ
+print(parrot[0])#WORKS IN INTELLIJ
+print(parrot[3])#WORKS IN INTELLIJ
+print(parrot[-1])#WORKS IN INTELLIJ
 # print(parrot[14])#No charactpr pf that range 
-print(parrot[0:6])#WORKES IN INTELLIJ
-print(parrot[0:6]) # 0 is where to start 6 is the length amount #WORKES IN INTELLIJ
-print(parrot[6:])#WORKES IN INTELLIJ
-print(parrot[-4:-2])#WORKES IN INTELLIJ
-print(parrot[0:9:2])#WORKES IN INTELLIJ
-print(parrot[0::3])#WORKES IN INTELLIJ
+print(parrot[0:6])#WORKS IN INTELLIJ
+print(parrot[0:6]) # 0 is where to start 6 is the length amount #WORKS IN INTELLIJ
+print(parrot[6:])#WORKS IN INTELLIJ
+print(parrot[-4:-2])#WORKS IN INTELLIJ
+print(parrot[0:9:2])#WORKS IN INTELLIJ
+print(parrot[0::3])#WORKS IN INTELLIJ
 
 number = "9,223,372,036,854,775,807"
-print(number[1::4])#WORKES IN INTELLIJ
+print(number[1::4])#WORKS IN INTELLIJ
 
 numbers = "1, 2, 3, 4, 5, 6, 7, 8, 9,"
-print(numbers[0::3])#WORKES IN INTELLIJ
+print(numbers[0::3])#WORKS IN INTELLIJ
 
 string1 = "he's"
 string2 = "probably "
-print(string1 + string2)#WORKES IN INTELLIJ
-print(string1 + string2)#WORKES IN INTELLIJ
+print(string1 + string2)#WORKS IN INTELLIJ
+print(string1 + string2)#WORKS IN INTELLIJ
 
-print("he's probaly done")#WORKES IN INTELLIJ
+print("he's probaly done")#WORKS IN INTELLIJ
+
+#MULTIPLY STRINGS
+print("hello " *5)#PRINTS HELLO FIVE TIMES // #WORKS IN INTELLIJ
