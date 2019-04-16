c50c0a33d41b1837be128f851233a1f5f022c63f
CodeXRook <lacekitfly@gmail.com>
2019-04-15 22:47:40 -0400

    updated new test

--- myfirstcode.py
+++ myfirstcode.py
@@ -3,3 +3,5 @@ greeting = "whats up"
 name = "Xrook"
 print(greeting + ' ' + name)
 
+splitString = "This string has been\nsplit over\nserveral\nlines\nthis is the sixth line\nthis is the seventh"
+print(splitString)
