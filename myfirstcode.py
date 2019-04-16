a30cd3104a980a11b6f155979f1dbddcb5b69dde
CodeXRook <lacekitfly@gmail.com>
2019-04-15 23:01:27 -0400

    another split string

--- myfirstcode.py
+++ myfirstcode.py
@@ -6,7 +6,13 @@ print(greeting + ' ' + name)
 splitString = "This string has been\nsplit over\nserveral\nlines\nthis is the sixth line\n The change is at the end"
 print(splitString)
 
-tabbedString = "1\t2\t3\t4\t5\t6\t7"
+tabbedString = "1\t2\t3\t4\t5\t6\t7" 
 print(tabbedString)
 
-print(' The pet shop owner said "No, no\'e\'s uh...he\'s resting" ')
+print(' The pet shop owner said "No, no\'e\'s uh...he\'s resting" ')#resting split method
+print("The pet shop owmer said \"No, no, 'e's uh,...he's resting\"")#another split method
+anotherSplitString ="""This string has been 
+split over
+several lines"""
+print(anotherSplitString)
+
