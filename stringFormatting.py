48b123a7e0ef02775c6050e91e14723a91161fec
CodeXRook <lacekitfly@gmail.com>
2019-04-16 02:54:43 -0400

    print days plus fomate

--- stringFormatting.py
+++ stringFormatting.py
@@ -1,4 +1,6 @@
 age = 24
 print("My age is" + str(age) +' '"years")#WORKS IN INTELLIJ
 
-print("My age is {0} years".format(age))
+print("My age is {0} years".format(age))#WORKS IN INTELLIJ
+
+print("There are {0} days in {1}, {2}, {3}, {4}, {5} {6} and {7}".format(31, "January", "March", "May", "July", "August", "October", "December"))#WORKS IN INTELLIJ
