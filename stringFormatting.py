0cb707e090772ba79cc538db01f6dbb1c43aa4d3
CodeXRook <lacekitfly@gmail.com>
2019-04-16 04:02:05 -0400

    replacement fields

--- stringFormatting.py
+++ stringFormatting.py
@@ -36,3 +36,4 @@ for i in range(1, 12):
     print("No. {0:2} squared is {1:<4} and cubed is {2:<4}".format(i, i ** 2, i ** 3))
 
     print("Pi is approximately {0:12.50f}".format(22 / 7))
+    print("Pi is approximately {0:52.50f}".format(22 / 7))
