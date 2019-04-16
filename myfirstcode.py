73c625cf9d9c270e7a87061f76e89e3acda07b10
CodeXRook <lacekitfly@gmail.com>
2019-04-15 23:08:10 -0400

    split string method

--- myfirstcode.py
+++ myfirstcode.py
@@ -11,8 +11,13 @@ print(tabbedString)
 
 print(' The pet shop owner said "No, no\'e\'s uh...he\'s resting" ')#resting split method
 print("The pet shop owmer said \"No, no, 'e's uh,...he's resting\"")#another split method
-anotherSplitString ="""This string has been 
+anotherSplitString = """This string has been 
 split over
-several lines"""
+several lines """
 print(anotherSplitString)
+
+print('''The pet shop owner said "No, no,'e's' uh,...he's resting"''')
+
+print("Number  1\tThe Larch")# Number  1 The Larch
+print("Number  2\tThe Horse Chestnut")#Number  2	The Horse Chestnut
 
