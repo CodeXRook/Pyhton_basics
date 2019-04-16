eef7c608817ebbca51767c183ed7b24c02d5ac88
CodeXRook <lacekitfly@gmail.com>
2019-04-15 22:56:43 -0400

    the \ creates a string split because of the open quotes

--- myfirstcode.py
+++ myfirstcode.py
@@ -3,5 +3,10 @@ greeting = "whats up"
 name = "Xrook"
 print(greeting + ' ' + name)
 
-splitString = "This string has been\nsplit over\nserveral\nlines\nthis is the sixth line\nthis is the seventh"
+splitString = "This string has been\nsplit over\nserveral\nlines\nthis is the sixth line\n The change is at the end"
 print(splitString)
+
+tabbedString = "1\t2\t3\t4\t5\t6\t7"
+print(tabbedString)
+
+print(' The pet shop owner said "No, no\'e\'s uh...he\'s resting" ')
