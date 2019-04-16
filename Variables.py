ede3d48a10cced286fe3982e11b759f26e138b40
CodeXRook <lacekitfly@gmail.com>
2019-04-16 01:42:26 -0400

    updated test pass, with #WORKS IN INTELLIJ

--- Variables.py
+++ Variables.py
@@ -1,23 +1,25 @@
 parrot = "Nowregian Blue"
-print(parrot)
-print(parrot[0])
-print(parrot[3])
-print(parrot[-1])
+print(parrot)#WORKES IN INTELLIJ
+print(parrot[0])#WORKES IN INTELLIJ
+print(parrot[3])#WORKES IN INTELLIJ
+print(parrot[-1])#WORKES IN INTELLIJ
 # print(parrot[14])#No charactpr pf that range 
-print(parrot[0:6])
-print(parrot[0:6]) # 0 is where to start 6 is the length amount
-print(parrot[6:])
-print(parrot[-4:-2])
-print(parrot[0:9:2])
-print(parrot[0::3])
+print(parrot[0:6])#WORKES IN INTELLIJ
+print(parrot[0:6]) # 0 is where to start 6 is the length amount #WORKES IN INTELLIJ
+print(parrot[6:])#WORKES IN INTELLIJ
+print(parrot[-4:-2])#WORKES IN INTELLIJ
+print(parrot[0:9:2])#WORKES IN INTELLIJ
+print(parrot[0::3])#WORKES IN INTELLIJ
 
 number = "9,223,372,036,854,775,807"
-print(number[1::4])
+print(number[1::4])#WORKES IN INTELLIJ
 
 numbers = "1, 2, 3, 4, 5, 6, 7, 8, 9,"
-print(numbers[0::3])
+print(numbers[0::3])#WORKES IN INTELLIJ
 
 string1 = "he's"
 string2 = "probably "
-print(string1 + string2)
-print(string1 + string2)
+print(string1 + string2)#WORKES IN INTELLIJ
+print(string1 + string2)#WORKES IN INTELLIJ
+
+print("he's probaly done")#WORKES IN INTELLIJ
