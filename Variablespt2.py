df66ba00ab23a9c0d085e6431964e4a1527c950b
CodeXRook <lacekitfly@gmail.com>
2019-04-16 02:21:25 -0400

    item_price

--- Variablespt2.py
+++ Variablespt2.py
@@ -2,4 +2,5 @@
 # set them to strings that represent 5 common shopping list items 
 #I DID THIS ALREADY, SO TIME I'LL BE DOING THE INPUT SO I BETTER UNDERSTAND THE USAGE.
 
-item_name = input('Add to list')
+item_name_1 = input('Add to list') #WORKS IN INTELLIJ
+item_price_1 = input('Whats your spending budget?') #WORKS IN INTELLIJ
